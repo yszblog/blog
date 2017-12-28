aaf1a4ca9d2e34a7f7f08af4fd649fd982daa0ff
ysz <976776565@qq.com>
2017-12-28 11:05:45 +0800

    相册配置修改

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -129,7 +129,7 @@
   
             liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                   <a href="' + src + '" itemprop="contentUrl" data-size="'+ size +'" data-type="' + type + '" data-target="' + src + '">\
-                    <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="./assets/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                    <img width="250" height="250" class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="./assets/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                   </a>\
                   <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
               </figure>';
