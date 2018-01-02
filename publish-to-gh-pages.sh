3f83d5c1421ab92d8189806c5b32497b6d235b09
ysz <976776565@qq.com>
2018-01-02 20:04:02 +0800

    修改配置文件，添加友言

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -9,6 +9,6 @@ cd ./public
 git config user.name  "git-ysz" # 这里改成自己的 Github 用户名
 git config user.email "976776565@qq.com" # 这里改成自己的 Github 邮箱
 # add commit timestamp
-git add .
+git add . 
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 git push --force --quiet "https://${api}@${GH_REF}" master:master # github_token 改成你在 Travis CI 中添加的环境变量名称
